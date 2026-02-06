eb133777950dc8a83d8e5b8ce8192f36dd42c921
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-02-06 15:27:22 +0100

    dtoRepair

---
+++ src/main/java/com/svalero/ecorepair/dto/RepairOutDto.java
@@ -0,0 +1,35 @@
+package com.reparaciones.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RepairOutDto {
+
+    private long id;
+
+    @NotBlank(message = "La descripción es obligatoria")
+    @Size(max = 200)
+    private String description;
+
+    @NotNull(message = "El coste es obligatorio")
+    @Min(value = 0, message = "El coste debe ser positivo")
+    private Double cost;
+
+    @NotNull(message = "La fecha de reparación es obligatoria")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate repairDate;
+
+    @NotNull(message = "Debes indicar si la reparación está finalizada")
+    private Boolean repair;
+
+    @NotNull(message = "El deviceId es obligatorio")
+    private Long deviceId;
+}
