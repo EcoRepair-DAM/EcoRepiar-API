b38cd0a4c0b0bb4db45416a8526831e45bec4077
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-05-15 13:36:23 +0200

    rol y registro, update role

---
+++ src/main/java/com/svalero/ecorepair/dto/RegisterRequest.java
@@ -0,0 +1,21 @@
+package com.svalero.ecorepair.dto;
+
+import com.svalero.ecorepair.domain.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @Email(message = "Email must be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must contain at least 6 characters")
+    private String password;
+
+    private UserRole role = UserRole.USER;
+}
